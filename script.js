7acd7990adbfe8ad2076c7132192377d6bdf8a83
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-12-06 01:11:00 +0000

    ...

--- script.js
+++ script.js
@@ -3,48 +3,78 @@ var play = "";
 var p1 = [];
 var p2 = [];
 var turns = 0;
+
+
+
 function play(id) {
   if (turn == 1) {
-    turns += 1;
-    document.getElementById(id).style.backgroundColor = "#00ff8c";
-    p1.push(id);
-    end();
+    if (p2.includes(id)) {
+      alert1('Invalid Move');
+    }
+    else {
+      turns += 1;
+      document.getElementById(id).style.backgroundColor = "#00ff8c";
+      p1.push(id);
+      end();
+    }
   }
   else if (turn == 2) {
-    turns += 1;
-    document.getElementById(id).style.backgroundColor = "#e600ff";
-    p2.push(id);
-    end();
+    if (p1.includes(id)) {
+      alert1('Invalid Move');
+    }
+    else {
+      turns += 1;
+      document.getElementById(id).style.backgroundColor = "#e600ff";
+      p2.push(id);
+      end();
+    }
   }
 }
+
+
+
 function end() {
   if ((p1.includes('r0c0') && p1.includes('r0c1') && p1.includes('r0c2')) || (p1.includes('r1c0') && p1.includes('r1c1') && p1.includes('r1c2')) || (p1.includes('r2c0') && p1.includes('r2c1') && p1.includes('r2c2')) || (p1.includes('r0c0') && p1.includes('r1c0') && p1.includes('r2c0')) || (p1.includes('r0c1') && p1.includes('r1c1') && p1.includes('r2c1')) || (p1.includes('r0c2') && p1.includes('r1c2') && p1.includes('r2c2')) || (p1.includes('r0c0') && p1.includes('r1c1') && p1.includes('r2c2')) || (p1.includes('r0c2') && p1.includes('r1c1') && p1.includes('r2c0'))) {
       document.getElementById('win').style.display = 'block';
       setTimeout(w2,2500)
   }
-      else if ((p2.includes('r0c0') && p2.includes('r0c1') && p2.includes('r0c2')) || (p2.includes('r1c0') && p2.includes('r1c1') && p2.includes('r1c2')) || (p2.includes('r2c0') && p2.includes('r2c1') && p2.includes('r2c2')) || (p2.includes('r0c0') && p2.includes('r1c0') && p2.includes('r2c0')) || (p2.includes('r0c1') && p2.includes('r1c1') && p2.includes('r2c1')) || (p2.includes('r0c2') && p2.includes('r1c2') && p2.includes('r2c2')) || (p2.includes('r0c0') && p2.includes('r1c1') && p2.includes('r2c2')) || (p2.includes('r0c2') && p2.includes('r1c1') && p2.includes('r2c0'))) {
-          document.getElementById('win').style.transform = 'rotate(-180deg)';
-          document.getElementById('win').style.display = 'block';
-          setTimeout(w1,2500)
-      }
-      else if (turns == 9) {
-        document.getElementById('draw').style.display = 'block';
+  else if ((p2.includes('r0c0') && p2.includes('r0c1') && p2.includes('r0c2')) || (p2.includes('r1c0') && p2.includes('r1c1') && p2.includes('r1c2')) || (p2.includes('r2c0') && p2.includes('r2c1') && p2.includes('r2c2')) || (p2.includes('r0c0') && p2.includes('r1c0') && p2.includes('r2c0')) || (p2.includes('r0c1') && p2.includes('r1c1') && p2.includes('r2c1')) || (p2.includes('r0c2') && p2.includes('r1c2') && p2.includes('r2c2')) || (p2.includes('r0c0') && p2.includes('r1c1') && p2.includes('r2c2')) || (p2.includes('r0c2') && p2.includes('r1c1') && p2.includes('r2c0'))) {
+      document.getElementById('win').style.transform = 'rotate(-180deg)';
+      document.getElementById('win').style.display = 'block';
+      setTimeout(w1,2500)
+  }
+  else if (turns == 9) {
+    document.getElementById('draw').style.display = 'block';
         setTimeout(newg,5000)
-      }
-  
+  }
 }
+
+
 
 function w1() {
   document.getElementById('win').style.display = 'none';
   document.getElementById('lose').style.display = 'block';
   setTimeout(newg,5000)
 }
+
+
+
 function w2() {
   document.getElementById('win').style.display = 'none';
   document.getElementById('lose').style.transform = 'rotate(-180deg)';
   document.getElementById('lose').style.display = 'block';
   setTimeout(newg,5000)
 }
+
+
+
 function newg() {
   location.reload();
+}
+
+
+
+function alert1(mes) {
+  document.getElementById('message2').innerHTML = mes;
+  $('#message2').fadeIn('slow', function(){$('#message2').delay(3000).fadeOut();});
 }
